6249944ec7f2f35881a122fd66a2482fc916e4a6
Waqar Rashid <waqar.rashid@voltstorage.com>
2023-01-03 11:32:33 +0100

    Added youtube iframe

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ function App() {
   return (
     <div className="App">
       <h1>Vite + React</h1>
+      <iframe src='https://www.youtube.com/embed/E7wJTI-1dvQ'
+        frameborder='0'
+        allow='autoplay; encrypted-media'
+        allowfullscreen
+        title='video'
+      />
     </div>
   )
 }
