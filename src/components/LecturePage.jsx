f15f3101f893c62a894e0509b8df378a26f22293
Waqar Rashid <waqar.rashid@voltstorage.com>
2023-01-04 18:05:28 +0100

    Implemented callbacks and added list of videos

--- src/components/LecturePage.jsx
+++ src/components/LecturePage.jsx
@@ -4,13 +4,38 @@ import { PageControls } from "./PageControls"
 import { VideoPlayer } from "./VideoPlayer"
 
 export default function LecturePage() {
-  const [source, setSource] = useState("https://www.youtube.com/embed/7EmboKQH8lM")
+  const [source, setSource] = useState(0)
+  const sources = [
+    "https://www.youtube.com/embed/RebA5J-rlwg",
+    "https://www.youtube.com/embed/7EmboKQH8lM",
+    "https://www.youtube.com/embed/lyNetvEfvT0",
+  ]
+
+  const nextVideo = () => {
+    console.log("Next clicked")
+    let newSource = source + 1
+    if (newSource => sources.length) {
+      newSource = 0
+    }
+
+    setSource(newSource)
+  }
+
+  const prevVideo = () => {
+    console.log("Prev clicked")
+    let newSource = source - 1
+    if (newSource < 0) {
+      newSource = sources.length - 1
+    }
+
+    setSource(newSource)
+  }
 
   return (
     <div className="App">
-      <VideoPlayer source={source} />
-      <PageControls />
-      <Comments source={source} />
+      <VideoPlayer source={sources[source]} />
+      <PageControls nextFn={nextVideo} prevFn={prevVideo} />
+      <Comments source={sources[source]} />
     </div>
   )
 }
